195dcbc098d0a815397c263f3ad1be3c8f15222c
E-duardo-Lucas <147790356+E-duardo-Lucas@users.noreply.github.com>
2024-07-29 18:56:21 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -390,6 +390,30 @@ const app = new Vue({
             // Eliminar los datos de lukiControl
             localStorage.removeItem('lukiControl2');
             alert('LocalStorage VACÍA');
+        },
+        toggleFullScreen() {
+            const elem = document.documentElement;
+            if (!document.fullscreenElement) {
+              if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+              } else if (elem.mozRequestFullScreen) { // Firefox
+                elem.mozRequestFullScreen();
+              } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+                elem.webkitRequestFullscreen();
+              } else if (elem.msRequestFullscreen) { // IE/Edge
+                elem.msRequestFullscreen();
+              }
+            } else {
+              if (document.exitFullscreen) {
+                document.exitFullscreen();
+              } else if (document.mozCancelFullScreen) { // Firefox
+                document.mozCancelFullScreen();
+              } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+                document.webkitExitFullscreen();
+              } else if (document.msExitFullscreen) { // IE/Edge
+                document.msExitFullscreen();
+              }
+            }
         }
     },
     computed: {
